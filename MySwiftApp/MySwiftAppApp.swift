8d6b3209f0825fd0f834417e0d497a310b439ea9
SotaZaima <ss.34r.0061@s.thers.ac.jp>
2024-04-24 10:58:05 +0900

    Initial Commit

---
+++ MySwiftApp/MySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftAppApp.swift
+//  MySwiftApp
+//
+//  Created by 財満創大 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
